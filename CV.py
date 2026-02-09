ace5710d38f92d3aec50a22482e0b8accb0924af
matys8090-maker <matys8090@gmail.com>
2026-02-09 22:45:10 +0100

    Add files via upload

---
+++ CV.py
@@ -0,0 +1,48 @@
+import streamlit as st
+
+st.title("NDEYE MATY SALL")
+
+with st.sidebar:
+    st.image("MATY.jpeg",width=200)
+
+    st.write("Ndeye Maty Sall")
+
+    st.subheader("ADRESSE")
+    st.write("jaxaay1")
+
+    st.subheader("AGE")
+    st.write("20ans")
+
+    st.subheader("CONTACTS")
+    st.write("tel:766941542")
+    st.write("email:matys8090@gmail.com")
+
+
+st.write("Technicienne supérieure en géomatique ")
+st.subheader("PROFIL")
+st.write("Geomaticienne passionnée,experte en analyse spatiale et cartographie numérique.je recherche un poste stimulant ou je pourrai à profit mes compétences en (Arcgis;Qgis...)pour contribuer à des projets innovant et précis")
+    
+st.subheader("FORMATIONS ET DIPLOMES")
+st.write("BFEM: cours privée L2S")
+st.write("BAC:cours privée L2S en 2024/2025")
+st.write("BTS geomatique")
+
+st.subheader("COMPETENCES")
+st.write("traitement de logiciel (ARCGIS,QGIS,autocad)")
+st.write("prgrammation")
+st.write("topographie")
+st.write("collecte de données")
+st.write("cartographie")
+
+st.subheader("EXPERIENCES")
+st.write("Analyse et traitement de données pour la cartographie de zones a risque")
+st.write("developpement d'applications géomatiques et d'outils de visualisation")
+
+st.subheader("LANGUES")
+st.write("anglais,francais,espagnol")
+
+st.subheader("PROJETS ACADEMIQUES")
+st.write("urbanisme,analyse environnementale et changement climatique,gestion des risques")
+
+st.subheader("DISPONIBILITE")
+st.write("disponible immédiatement")
